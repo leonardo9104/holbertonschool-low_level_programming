cfc9f24495d6e07c9233cdbe0db98bffe12ec36f
root <3518@holbertonschool.com>
2021-06-28 20:07:10 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - positive negative
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
