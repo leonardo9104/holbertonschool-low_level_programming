0c87266c3e3d84cb3be4b46064bf13635552279a
leonardo9104 <3518@holbertonschool.com>
2021-06-30 19:06:56 -0500

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - positive negative
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
