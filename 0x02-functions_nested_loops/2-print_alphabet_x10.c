e7bd43ee701a0095d5b544f52a82af23817c6db7
leonardo9104 <3518@holbertonschool.com>
2021-06-30 22:27:31 -0500

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* print_alphabet_x10- check the code for Holberton School students
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	char n;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+	}
+
+	_putchar('\n');
+}
